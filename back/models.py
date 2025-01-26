f7185c1f54bfeff254675ebd92ba7388961e5178
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2025-01-26 18:45:22 -0500

    it kinda works?

---
+++ back/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Date
+from sqlalchemy.orm import relationship
+from database import Base
+
+class USUARIO(Base):
+    __tablename__ = "USUARIO"
+
+    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nombre_usuario = Column(String(255), nullable=False)
+    contrasenia = Column(String(255), nullable=False)
+    imagen_perfil = Column(Text, default="default_profile_image.png")
+    tareas = relationship("TAREA", back_populates="usuario")
+
+class CATEGORIA(Base):
+    __tablename__ = "CATEGORIA"
+
+    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nombre = Column(String(255), nullable=False)
+    description = Column(Text)
+    tareas = relationship("TAREA", back_populates="categoria")
+
+class TAREA(Base):
+    __tablename__ = "TAREA"
+
+    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    texto_tarea = Column(Text, nullable=False)
+    fecha_creacion = Column(Date, nullable=False)
+    fecha_tentativa_finalizacion = Column(Date)
+    estado = Column(String(50), nullable=False)
+
+    ID_Usuario = Column(Integer, ForeignKey("USUARIO.ID", ondelete="CASCADE"), nullable=False)
+    ID_Categoria = Column(Integer, ForeignKey("CATEGORIA.ID", ondelete="SET NULL"))
+
+    usuario = relationship("USUARIO", back_populates="tareas")
+    categoria = relationship("CATEGORIA", back_populates="tareas")
--- front/main.py
+++ front/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+import sys
+sys.path.append('./back')
+import models
+import database
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=database.engine)
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+def get_usuarios():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class UsuarioResponse(BaseModel):
+    ID: int
+    nombre_usuario: str
+    imagen_perfil: str
+
+    class Config:
+        from_attributes = True  # Enable ORM mode for Pydantic
+@app.get("/", response_model=List[UsuarioResponse])
+def get_all_usuarios(db: Session = Depends(get_usuarios)):
+    usuarios = db.query(models.USUARIO).all()
+    return usuarios
+
